08a2396710cce6074b6d87b10c8503eb92850ed8
Hauke Stieler <mail@hauke-stieler.de>
2023-08-01 13:48:52 +0200

    Implement simple statically sized encoded area determination

--- cobi/encoding.go
+++ cobi/encoding.go
@@ -17,28 +17,71 @@ func (e *EncodedArea) Contains(x, y int) bool {
 
 // Encode determines the encoded areas per color channel R (0), G (1), B (2) and A (3).
 func Encode(img image.Image) ([4][]EncodedArea, error) {
-	for y := 0; y < img.Width; y++ {
-		for x := 0; x < img.Height; x++ {
-		}
-	}
-
-	return [4][]EncodedArea{}, nil
+	return [4][]EncodedArea{
+		EncodeChannel(img.R),
+		EncodeChannel(img.G),
+		EncodeChannel(img.B),
+		EncodeChannel(img.A),
+	}, nil
 }
 
 func EncodeChannel(values [][]byte) []EncodedArea {
 	var result []EncodedArea
 
+	for {
+		area := findLargestNonEncodedArea(values, result)
+		if area == nil {
+			break
+		}
+		result = append(result, *area)
+	}
+
 	return result
 }
 
-// getMinUncoveredPixel determines the smallest pixel that is not covered by any area. It is assumed that the encoded
+// findLargestNonEncodedArea finds the next encoded area following the strategy to find areas from the upper-left to the
+// bottom-right of the image.
+func findLargestNonEncodedArea(values [][]byte, areas []EncodedArea) *EncodedArea {
+	width := len(values)
+	height := len(values[0])
+
+	minX, minY := findMinUncoveredPixel(areas, width, height)
+	if minX == -1 || minY == -1 {
+		return nil
+	}
+
+	// TODO use real interpolation methods to find encoded areas
+	w := 8
+	if minX+w >= width {
+		w = width - 1 - minX
+	}
+	h := 8
+	if minY+h >= height {
+		h = height - 1 - minY
+	}
+
+	return &EncodedArea{
+		X: minX,
+		Y: minY,
+		W: w,
+		H: h,
+		Values: [4]byte{
+			values[minX][minY],
+			values[minX+w][minY],
+			values[minX][minY+h],
+			values[minX+w][minY+h],
+		},
+	}
+}
+
+// findMinUncoveredPixel determines the smallest pixel that is not covered by any area. It is assumed that the encoded
 // areas grow from the upper-left to the bottom-right. This means for example, when (3, 5) is the first non-covered
-// pixel, there is no other pixel p with p.X <= 3 && p.Y <= 5.
-func getMinUncoveredPixel(areas []EncodedArea, width, height int) (int, int) {
+// pixel, all pixels in rows 0, 1 or 2 are covered and all pixels of row 3 in columns 0-4 are covered.
+func findMinUncoveredPixel(areas []EncodedArea, width, height int) (int, int) {
 	x := 0
 	y := 0
 	for ; y < height; y++ {
-		for ; x < height; x++ {
+		for ; x < width; x++ {
 			// Assume this pixel (x, y) is not covered and return (x, y) if it indeed isn't covered.
 			isCovered := false
 			for _, area := range areas {
